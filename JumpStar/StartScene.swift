9ca1775423ad78e15ef0baf277743fe3cf152fc5
Pragun Sharma <pragunsharma@praguns-mbp.attlocal.net>
2017-12-21 15:18:25 -0800

    Added Tutorial Screen

--- JumpStar/StartScene.swift
+++ JumpStar/StartScene.swift
@@ -15,12 +15,19 @@ class StartScene: SKScene {
     
     
     var startLabel = SKLabelNode()
+    var DescpLabel_1 = SKLabelNode()
+    var DescpLabel_2 = SKLabelNode()
+    var DescpLabel_3 = SKLabelNode()
+    var PowerUpLabel_1 = SKLabelNode()
+    var PowerUpLabel_2 = SKLabelNode()
+    
     var gameScene: GameScene!
 
     
     
     let texture = [SKTexture(imageNamed: "Wall"), SKTexture(imageNamed: "backbtn"), SKTexture(imageNamed: "Ground"), SKTexture(imageNamed: "idle_1"), SKTexture(imageNamed: "idle_2"), SKTexture(imageNamed: "large_stack"), SKTexture(imageNamed: "Night_Sky"), SKTexture(imageNamed: "powerupYellow_bolt"), SKTexture(imageNamed: "powerupYellow_shield"), SKTexture(imageNamed: "powerupYellow_star"), SKTexture(imageNamed: "Restart_Btn"), SKTexture(imageNamed: "run_0"), SKTexture(imageNamed: "run_1"), SKTexture(imageNamed: "run_2"),
                    SKTexture(imageNamed: "run_3"), SKTexture(imageNamed: "run_4"), SKTexture(imageNamed: "run_5"), SKTexture(imageNamed: "shareIcon"), SKTexture(imageNamed: "star_coin"), SKTexture(imageNamed: "swim_0"), SKTexture(imageNamed: "swim_1"), SKTexture(imageNamed: "swim_2"), SKTexture(imageNamed: "swim_3"), SKTexture(imageNamed: "swim_4"), SKTexture(imageNamed: "swim_5") ]
+    
     
     override func didMove(to view: SKView) {
         
@@ -42,6 +49,57 @@ class StartScene: SKScene {
         startLabel.text = "TAP TO START!"
         startLabel.fontSize = 50
         self.addChild(startLabel)
+        
+        
+        DescpLabel_1.position = CGPoint(x: self.frame.width/2, y: self.frame.height/2 + 40)
+        DescpLabel_1.zPosition = 2
+        DescpLabel_1.text = "Drag up to Jump."
+        DescpLabel_1.horizontalAlignmentMode = .center
+        DescpLabel_1.fontSize = 20
+        self.addChild(DescpLabel_1)
+        
+        DescpLabel_2.position = CGPoint(x: self.frame.width/2, y: self.frame.height/2 + 10)
+        DescpLabel_2.zPosition = 2
+        DescpLabel_2.text = "Double Tap to perform moonwalk."
+        DescpLabel_2.fontSize = 20
+        DescpLabel_2.horizontalAlignmentMode = .center
+        self.addChild(DescpLabel_2)
+        
+        DescpLabel_3.position = CGPoint(x: self.frame.width/2, y: self.frame.height/2 - 20)
+        DescpLabel_3.zPosition = 2
+        DescpLabel_3.text = "Single Tap to control velocity in air."
+        DescpLabel_3.horizontalAlignmentMode = .center
+        DescpLabel_3.fontSize = 20
+        self.addChild(DescpLabel_3)
+        
+        let shieldPwrUp = SKSpriteNode(imageNamed: "powerupYellow_shield")
+        shieldPwrUp.position = CGPoint(x: self.frame.width/2 - 120 , y: self.frame.height/2 - 50)
+        shieldPwrUp.setScale(0.7)
+        shieldPwrUp.zPosition = 6
+        self.addChild(shieldPwrUp)
+        
+        PowerUpLabel_1.position = CGPoint(x: shieldPwrUp.position.x + 130, y: shieldPwrUp.position.y - 5)
+        PowerUpLabel_1.zPosition = 2
+        PowerUpLabel_1.text = "Shield: Makes you Idestructible."
+        PowerUpLabel_1.fontSize = 15
+        self.addChild(PowerUpLabel_1)
+        
+        let magnetPowerUp = SKSpriteNode(imageNamed: "powerupYellow_star")
+        magnetPowerUp.position = CGPoint(x: self.frame.width/2 - 120 , y: self.frame.height/2 - 100)
+        magnetPowerUp.setScale(0.7)
+        magnetPowerUp.zPosition = 6
+        self.addChild(magnetPowerUp)
+        
+        PowerUpLabel_2.position = CGPoint(x: magnetPowerUp.position.x + 110, y: magnetPowerUp.position.y - 5)
+        PowerUpLabel_2.zPosition = 2
+        PowerUpLabel_2.text = "Magnet: Attracts Coins."
+        PowerUpLabel_2.fontSize = 15
+        self.addChild(PowerUpLabel_2)
+        
+        
+
+        
+        
         
         let ground = SKSpriteNode(imageNamed: "Ground")
         ground.setScale(0.5)
